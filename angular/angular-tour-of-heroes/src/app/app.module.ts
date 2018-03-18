12ce74e1a569d1c13eea8a6e11e951e601d78a5d
Duc Nguyen <vanducld@gmail.com>
2018-03-19 05:53:21 +0700

    finish http

--- angular/angular-tour-of-heroes/src/app/app.module.ts
+++ angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 
 import { AppComponent } from './app.component';
@@ -12,6 +16,7 @@ import { MessageService } from './message.service';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 
 @NgModule({
@@ -21,16 +26,26 @@ import { NotFoundComponentComponent } from './not-found-component/not-found-comp
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    NotFoundComponentComponent
+    NotFoundComponentComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation : false}
+    ),
+    HttpClientModule
   ],
   providers: [
     HeroService,
-    MessageService
+    MessageService,
+    InMemoryDataService
   ],
   bootstrap: [AppComponent]
 })
