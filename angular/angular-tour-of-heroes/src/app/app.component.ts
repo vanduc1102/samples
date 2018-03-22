16846afa8e02a07aa30ff204a9d6c1f6437a27fd
Duc Nguyen <vanducld@gmail.com>
2018-03-22 16:20:53 +0700

    finish httpclient angular

--- angular/angular-tour-of-heroes/src/app/app.component.ts
+++ angular/angular-tour-of-heroes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor (
+    public translate: TranslateService
+  ) {
+    // translate.setDefaultLang('en');
+    translate.use('vi');
+  }
+
 }
 
 
