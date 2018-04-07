bdfed8a0bb44039d913cf3037696b46b4da267cd
Duc Nguyen <vanducld@gmail.com>
2018-04-07 14:59:22 +0700

    Correct winston log

--- nodejs/winston-test/logger.js
+++ nodejs/winston-test/logger.js
@@ -1,9 +1,10 @@
 'use strict';
+
 const winston = require('winston');
 const DailyRotateFile = require('winston-daily-rotate-file');
-const path = require('path'),
-fs = require('fs'),
-logLevel =  process.env.LOG_LEVEL || process.env.NODE_ENV !== 'production' ? 'debug' : 'info';
+const path = require('path');
+const fs = require('fs');
+const logLevel =  process.env.LOG_LEVEL || process.env.NODE_ENV !== 'production' ? 'debug' : 'info';
 
 const logFolder = path.resolve(path.dirname(require.main.filename), 'log');
 
@@ -11,24 +12,37 @@ if(!fs.existsSync(logFolder)){
 	fs.mkdirSync(logFolder);
 }
 
-const transportDailyRotateFile = new (winston.transports.DailyRotateFile)({
-	dirname: logFolder,
-    filename: './log',
-    datePattern: 'yyyy-MM-dd.',
-	prepend: true,
-	maxsize: 1024,
-    level: logLevel
-  });
+function getLabel (callingModule) {
+	if(!callingModule){
+		return '';
+	}
+	let parts = callingModule.filename.split('/');
+	return parts[parts.length - 2] + '/' + parts.pop();
+};
 
-const logger = new (winston.Logger)({
-	level: logLevel,
-	transports: [
-		transportDailyRotateFile
-	]
-});
+function createLogger(callingModule) {
+	let logger  = new winston.Logger({
+		transports: [
+			new (winston.transports.DailyRotateFile)({
+				name:'rotate-file-logger',
+				dirname: logFolder,
+				filename: './log',
+				datePattern: 'yyyy-MM-dd.',
+				prepend: true,
+				maxsize: 1024 * 1024 * 5,
+				level: logLevel,
+				label: getLabel(callingModule)
+			}),
+			new (winston.transports.Console)({
+				name:'console-logger',
+				label: getLabel(callingModule)
+			})
+		]
+	});
+	if (process.env.NODE_ENV !== 'production') {
+		logger.remove('rotate-file-logger')
+	}
+	return logger;
+};
 
-if (process.env.NODE_ENV !== 'production') {
-	logger.add(winston.transports.Console);
-}
-
-module.exports = logger;
+module.exports = createLogger;
