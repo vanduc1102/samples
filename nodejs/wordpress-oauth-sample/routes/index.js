31b7387b4ec7b38add903f559243f55226afbe8f
DUC NGUYEN <vanducld@gmail.com>
2021-04-13 14:27:45 +0700

    User FORM to prevent showing secKey on URL

--- nodejs/wordpress-oauth-sample/routes/index.js
+++ nodejs/wordpress-oauth-sample/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Rokt 2021' });
+  res.render('index', { title: 'Hello 2021' });
 });
 
 module.exports = router;
--- nodejs/wordpress-oauth-sample/routes/oauth.js
+++ nodejs/wordpress-oauth-sample/routes/oauth.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
-const querystring = require('querystring');
+var crypto = require("crypto");
 
 const ONE_HOUR = 60 * 60 * 1000;
 
@@ -9,7 +9,7 @@ router.get('/start', function (req, res, next) {
   console.log("start: ", req.query);
   const redirect_uri = req.query.redirect_uri;
   const callbackUrl = 'http://localhost:3000/oauth/callback'
-  const oauthUrl = 'https://public-api.wordpress.com/oauth2/authorize?client_id='+process.env.CLIENT_ID+'&response_type=code&scope=auth&redirect_uri=' + callbackUrl;
+  const oauthUrl = 'https://public-api.wordpress.com/oauth2/authorize?client_id=' + process.env.CLIENT_ID + '&response_type=code&scope=auth&redirect_uri=' + callbackUrl;
 
   res.cookie('redirect_uri', redirect_uri, {
     secure: true,
@@ -31,8 +31,33 @@ router.get('/callback', async function (req, res, next) {
   const accessTokenResponse = await getAccessToken(code);
   console.log("Wordpress OAuth response: ", accessTokenResponse.data);
   console.log('read cookie - redirect_uri: ', req.cookies['redirect_uri']);
-  res.redirect(redirect_uri + '&' + querystring.stringify(accessTokenResponse.data));
+  const storeOrigin = getStoreOrigin(redirect_uri);
+  const authEndpoint = storeOrigin + '/wp-json/sample-woocommerce/authentication';
+  var secretApiKey = crypto.randomBytes(32).toString('hex');
+
+  // TODO persist storeOrigin, authEndpoint to Database
+  res.send(
+    getSaveAuthenticationForm(authEndpoint, secretApiKey)
+  )
 });
+
+function getSaveAuthenticationForm(authenticationEndpoint, secretApiKey) {
+  return `
+    <form name="save-auth-form" action="${authenticationEndpoint}" method="POST">
+      <input type="hidden" name="secret_api_key" value="${secretApiKey}" />
+    </form>
+    <script>
+      window.onload=function(){
+        document.forms["save-auth-form"].submit();
+      }
+    </script>
+    `
+}
+
+function getStoreOrigin(url) {
+  const redirectUrl = new URL(url);
+  return redirectUrl.origin;
+}
 
 async function getAccessToken(tokenCode) {
   const formData = new URLSearchParams()
