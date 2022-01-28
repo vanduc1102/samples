4032be933bf88508902e805db9803c513bd3289d
DUC NGUYEN <vanducld@gmail.com>
2022-01-28 13:44:25 +0700

    Support compile with imports

--- reactjs/cra-solidity-etherjs/src/components/SmartContract/index.tsx
+++ reactjs/cra-solidity-etherjs/src/components/SmartContract/index.tsx
@@ -1,43 +1,61 @@
 import React, { useState } from 'react';
 
-import { Button, Input, notification } from 'antd';
+import { Button, Input, notification, Select } from 'antd';
 import { ContractFactory } from 'ethers';
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
+import { baseURLBin, compile, CompilerAbstract, pathToURL } from '@remix-project/remix-solidity';
 
-import { DEFAULT_CONTRACT } from '../../configurations/contract';
-import workerService from '../../services/worker'
-import {
-    getContractABI,
-    getContractBinary,
-} from '../../utils/contract';
+import { TEST_CONTRACTS } from '../../configurations/contract';
+import { handleNpmImport } from '../../utils/content-resolver'
 
 import './index.css'
-const { TextArea } = Input;
 
-const CONTRACT_FILE_NAME = 'SimpleStorage.sol';
+const { TextArea } = Input;
+const { Option } = Select;
+
+
+const DEFAULT_VERSION = "soljson-v0.8.11+commit.d7f03943.js";
+const CONTRACT_FILE_NAME = 'MyContract.sol';
+const CONTRACT_NAME = 'MyContract';
+
+(function initSupportedSolcVersion() {
+    (pathToURL as any)[DEFAULT_VERSION] = baseURLBin;
+    (pathToURL as any)[DEFAULT_VERSION] = baseURLBin;
+})();
+
 
 const SmartContract: React.FC = () => {
     const [compiling, setCompiling] = useState(false);
     const [deploying, setDeploying] = useState(false);
-    const [source, setSource] = useState(DEFAULT_CONTRACT);
-    const [compileResult, setCompileResult] = useState('');
+    const [source, setSource] = useState(TEST_CONTRACTS[0].content);
+    const [compileResult, setCompileResult] = useState<Record<string, any>>();
     const { library, active } = useWeb3React<Web3Provider>();
 
     const handleCompile = async () => {
         setCompiling(true);
         try {
-            const compileResult = await workerService.compileContract({
-                command: 'compile',
-                content: source,
-                contractFileName: CONTRACT_FILE_NAME,
-                solidityVersion: 'v0.8.10+commit.fc410830'
-            });
+            const response = await compile(
+                {
+                    [CONTRACT_FILE_NAME]: {
+                        content: source
+                    }
+                }
+                , {
+                    version: DEFAULT_VERSION
+                }, handleNpmImport) as CompilerAbstract;
+
+            if (response.data.errors) {
+                notification.error({
+                    message: response.data.errors[0].formattedMessage
+                })
+                return;
+            }
             notification.success({
                 message: "Complied successfully, please check console.log for compilation result"
             })
-            console.log("compileResult: ", compileResult);
-            setCompileResult(compileResult);
+            console.log("All contract compileResult: ", response);
+            setCompileResult(response.getContract(CONTRACT_NAME));
         } finally {
             setCompiling(false);
         }
@@ -46,19 +64,11 @@ const SmartContract: React.FC = () => {
     const handleDeploy = async () => {
         setDeploying(true);
         try {
-            const contractBinary = getContractBinary(
-                compileResult,
-                CONTRACT_FILE_NAME,
-                "SimpleStorage"
-            );
-
-            const contractABI = getContractABI(
-                compileResult,
-                CONTRACT_FILE_NAME,
-                "SimpleStorage"
-            );
+            const contractBinary = '0x' + compileResult?.object.evm.bytecode.object;
+            const contractABI = compileResult?.object.abi;
 
             const signer = library?.getSigner();
+
             const contractFactory: ContractFactory = new ContractFactory(
                 contractABI,
                 contractBinary,
@@ -76,12 +86,24 @@ const SmartContract: React.FC = () => {
         }
     }
 
+    const handleContractChange = (value: string) => {
+        const foundContract = TEST_CONTRACTS.find(contract => contract.name === value);
+        if (foundContract) {
+            setSource(foundContract.content)
+        }
+    }
+
     return <div className='Container'>
         <div className='Left'>
-            <TextArea rows={4} value={source} onChange={(event) => setSource(event.target.value)} />
+            <Select defaultValue={TEST_CONTRACTS[0].name} onChange={handleContractChange} style={{ width: '250px' }}>
+                {TEST_CONTRACTS.map((contract) => (
+                    <Option key={contract.name}>{contract.name}</Option>
+                ))}
+            </Select>
+            <TextArea className='CodeEditor' autoSize value={source} showCount onChange={(event) => setSource(event.target.value)} />
         </div>
         <div className='Right'>
-            <Button className='Button' onClick={handleCompile} type='primary' loading={compiling} >Compile</Button>
+            <Button className='Button' onClick={handleCompile} type='primary' loading={compiling} disabled={!source} >Compile</Button>
             <Button className='Button' onClick={handleDeploy} danger disabled={!active || compiling || !compileResult} loading={deploying}>Deploy</Button>
         </div>
     </div>;
--- reactjs/cra-solidity-etherjs/src/configurations/contract.ts
+++ reactjs/cra-solidity-etherjs/src/configurations/contract.ts
@@ -1,9 +1,8 @@
-
-export const DEFAULT_CONTRACT = `
+export const TEST_CONTRACT_1 = `
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
 
-contract SimpleStorage {
+contract MyContract {
     uint storedData;
 
     function set(uint x) public {
@@ -15,3 +14,49 @@ contract SimpleStorage {
     }
 }
 `;
+
+export const TEST_CONTRACT_2 = `
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyContract is ERC721, ERC721Enumerable, Ownable {
+    constructor() ERC721("MyToken", "MTK") {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
+`;
+
+export const TEST_CONTRACTS = [
+  {
+    name: "Simple Contract",
+    content: TEST_CONTRACT_1,
+  },
+  {
+    name: "ECR71 - With imports",
+    content: TEST_CONTRACT_2,
+  },
+];
