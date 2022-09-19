16ec261140b88688b52340af9c07a4151d1f089f
DUC NGUYEN <vanducld@gmail.com>
2022-09-19 10:06:58 +0700

    Add logger

--- docker/print-envs/server.js
+++ docker/print-envs/server.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const morgan = require('morgan');
 
 // Constants
 const PORT = 8080;
@@ -9,11 +10,23 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
+app.set('trust proxy', true)
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(morgan('common'))
+
+app.get("/health", (req, res) => {
+    res.json({ data: { up: Date.now() } });
+});
+
 app.get("/envs", (req, res) => {
     res.json({ data: process.env });
+});
+
+app.get("/", (req, res) => {
+    res.json({ data: "Hello world!" });
 });
 
 app.listen(PORT, HOST);
