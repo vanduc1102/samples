1e533124909d4701fe61bbc1ed7db4639fec35bf
DUC NGUYEN <vanducld@gmail.com>
2022-09-30 15:00:26 +0700

    move to docker folder

---
+++ docker/web-print-envs/server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const packageJson = require('./package.json');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.set('trust proxy', true);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(morgan('common'));
+
+app.get('/health', (req, res) => {
+  res.json({data: {up: Date.now()}});
+});
+
+app.get('/info', (req, res) => {
+  res.json({data: packageJson});
+});
+
+app.get('/envs', (req, res) => {
+  res.json({data: process.env});
+});
+
+app.get('/', (req, res) => {
+  res.json({data: 'Hello world!'});
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`${new Date().toISOString()} - Running on http://${HOST}:${PORT}`);
+});
+
+module.exports = app;
